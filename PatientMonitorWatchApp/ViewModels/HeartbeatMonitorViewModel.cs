f33e652230ca3c07b4727c70de87ba14159ec736
NutToCrack <karol.orzechowski1337@gmail.com>
2022-10-09 02:42:13 +0200

    Even more clean up

--- PatientMonitorWatchApp/ViewModels/HeartbeatMonitorViewModel.cs
+++ PatientMonitorWatchApp/ViewModels/HeartbeatMonitorViewModel.cs
@@ -13,7 +13,7 @@ namespace PatientMonitorWatchApp.ViewModels
         public event EventHandler OnButtonClicked;
         public event EventHandler<int> OnDataReceived;
         Services.HeartRateMonitorService HRM;
-        // Services.NetworkAccessService Network = new Services.NetworkAccessService();
+        Services.NetworkAccessService Network = new Services.NetworkAccessService();
         private bool isUsed = true;
         private bool isDisposed = true;
         int HeartBeat;
@@ -46,7 +46,7 @@ namespace PatientMonitorWatchApp.ViewModels
                 reasumeButton = "Rozpocznij Pomiar";
                 HRM.SensorDataUpdated -= OnDataChanged;
                 HRM.Stop();
-                // SendData(results);
+                 //SendData(results);
                 results.Clear();
                 manageHRM();
             } else
@@ -73,10 +73,11 @@ namespace PatientMonitorWatchApp.ViewModels
             
         }
 
-        // public async void SendData(Dictionary<string, string> results) {
-        //     await Network.PostData(results);
-            // await Network.SendWebRequestSampleAsync();
-        // }
+        public async void SendData(Dictionary<string, string> results)
+        {
+            await Network.PostData(results);
+            await Network.SendWebRequestSampleAsync();
+        }
 
         private string prompt;
         private string reasumeButton = "Rozpocznij Pomiar";
